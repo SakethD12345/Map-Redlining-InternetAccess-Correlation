93fe8c7a378e0be8e5a39cd35e28f3ef38ed405b
channingbryant <channing_bryant@brown.edu>
2023-11-05 17:57:09 -0500

    frontend from gearup

--- front-end/src/MapBox.tsx
+++ front-end/src/MapBox.tsx
@@ -1,6 +1,6 @@
 import Map, { Layer, MapLayerMouseEvent, Source } from "react-map-gl";
 import { geoLayer, overlayData } from "./overlays";
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import { ACCESS_TOKEN } from "./private/api";
 
 interface LatLong {
@@ -9,41 +9,41 @@ interface LatLong {
 }
 
 function MapBox() {
-    const ProvidenceLatLong: LatLong = { lat: 41.824, long: -71.4128 };
-    const initialZoom = 10;
+  const ProvidenceLatLong: LatLong = { lat: 41.824, long: -71.4128 };
+  const initialZoom = 10;
 
-    function onMapClick(e: MapLayerMouseEvent) {
-        console.log(e.lngLat.lat);
-        console.log(e.lngLat.lng);
-    }
+  function onMapClick(e: MapLayerMouseEvent) {
+    console.log(e.lngLat.lat);
+    console.log(e.lngLat.lng);
+  }
 
-    const [viewState, setViewState] = useState({
-        longitude: ProvidenceLatLong.long,
-        latitude: ProvidenceLatLong.lat,
-        zoom: initialZoom,
-    });
+  const [viewState, setViewState] = useState({
+    longitude: ProvidenceLatLong.long,
+    latitude: ProvidenceLatLong.lat,
+    zoom: initialZoom,
+  });
 
-    const [overlay, setOverlay] = useState<GeoJSON.FeatureCollection | undefined>(
-        undefined
-    );
+  const [overlay, setOverlay] = useState<GeoJSON.FeatureCollection | undefined>(
+    undefined
+  );
 
-    useEffect(() => {
-        setOverlay(overlayData());
-    }, []);
-    return (
-        <Map
-            mapboxAccessToken={ACCESS_TOKEN}
-            {...viewState}
-            onMove={(ev) => setViewState(ev.viewState)}
-            style={{ width: window.innerWidth, height: window.innerHeight }}
-            mapStyle={"mapbox://styles/mapbox/streets-v12"}
-            onClick={(ev: MapLayerMouseEvent) => onMapClick(ev)}
-        >
-            <Source id="geo_data" type="geojson" data={overlay}>
-            <Layer {...geoLayer} />
-            </Source>
-        </Map>
-    )
+  useEffect(() => {
+    setOverlay(overlayData());
+  }, []);
+  return (
+    <Map
+      mapboxAccessToken={ACCESS_TOKEN}
+      {...viewState}
+      onMove={(ev) => setViewState(ev.viewState)}
+      style={{ width: window.innerWidth, height: window.innerHeight }}
+      mapStyle={"mapbox://styles/mapbox/streets-v12"}
+      onClick={(ev: MapLayerMouseEvent) => onMapClick(ev)}
+    >
+      <Source id="geo_data" type="geojson" data={overlay}>
+        <Layer {...geoLayer} />
+      </Source>
+    </Map>
+  );
 }
 
-export default MapBox
+export default MapBox;
