b977d866fcd7c6155416df592680274a48842d20
channingbryant <channing_bryant@brown.edu>
2023-11-08 18:21:02 -0500

    search box and for keyword search

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,11 +1,25 @@
 import "./App.css";
 import MapBox from "./MapBox";
-
+import InputBox from "./components/SearhKeyword";
+import { useState } from "react";
 
 function App() {
+  const [searchOverlay, setSearchOverlay] = useState<
+    GeoJSON.FeatureCollection | undefined
+  >(undefined);
+
+  const handleSetSearchOverlay = (
+    data: GeoJSON.FeatureCollection | undefined
+  ) => {
+    setSearchOverlay(data);
+  };
+
   return (
     <div className="App">
-      <MapBox/>
+      <MapBox />
+      <div>
+        <InputBox setState={handleSetSearchOverlay} />
+      </div>
     </div>
   );
 }
--- front-end/src/overlays.ts
+++ front-end/src/overlays.ts
@@ -1,18 +1,54 @@
 import { FeatureCollection } from "geojson";
-import { FillLayer } from "react-map-gl";
+import { FillLayer, LineLayer } from "react-map-gl";
+
+import {
+  isMapsServerResponse,
+  isServerErrorResponse,
+  MapsServerResponse,
+  ServerErrorResponse,
+} from "./utils/types";
 
 // Import the raw JSON file
-import rl_data from "./geodata/fullDownload.json"; 
+import rl_data from "./geodata/fullDownload.json";
 // you may need to rename the donwloaded .geojson to .json
 
-function isFeatureCollection(json: any): json is FeatureCollection {
+export function isFeatureCollection(json: any): json is FeatureCollection {
   return json.type === "FeatureCollection";
 }
 
 export function overlayData(): GeoJSON.FeatureCollection | undefined {
-  return (isFeatureCollection(rl_data)) ? rl_data : undefined;
+  return isFeatureCollection(rl_data) ? rl_data : undefined;
 }
 
+export const fetchData = async (url: string) => {
+  try {
+    const response = await fetch(url);
+
+    console.log("A response was received");
+    if (response == undefined) return undefined;
+
+    const responseJson: MapsServerResponse | ServerErrorResponse =
+      await response.json();
+
+    // Using Type Predicate to veriy that responseJson is a ServerResponse.
+    if (isMapsServerResponse(responseJson)) {
+      return responseJson.data;
+    } else if (isServerErrorResponse(responseJson)) {
+      return responseJson;
+      //alert("Could not get reponse from redlining data server: " + responseJson.message)
+    } else {
+      return responseJson;
+      //alert("Error: Unexpected response from redline data server.")
+
+      console.log(
+        "Error: Json returned is not of type MapsServerResponse or ServerErrorResponse"
+      );
+    }
+  } catch (e) {
+    console.log(e);
+    //alert("General error: " + e);
+  }
+};
 const propertyName = "holc_grade";
 export const geoLayer: FillLayer = {
   id: "geo_data",
@@ -32,5 +68,14 @@ export const geoLayer: FillLayer = {
       "#ccc",
     ],
     "fill-opacity": 0.2,
+  },
+};
+
+export const geoLayerLine: LineLayer = {
+  id: "geo_data2",
+  type: "line",
+  paint: {
+    "line-color": "#d11d1d",
+    "line-width": 1,
   },
 };
