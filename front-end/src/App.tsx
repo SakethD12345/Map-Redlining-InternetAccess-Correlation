9b4f7e4caf578ec4a94fa147b120a77eb1e1afb1
channingbryant <channing_bryant@brown.edu>
2023-11-09 19:45:53 -0500

    broaband string

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -2,12 +2,14 @@ import "./App.css";
 import MapBox from "./MapBox";
 import InputBox from "./components/SearchKeyword";
 import { useState } from "react";
+import BroadBandBox from "./components/BroadbandBox";
 
 function App() {
   const [searchOverlay, setSearchOverlay] = useState<
     GeoJSON.FeatureCollection | undefined
   >(undefined);
 
+  const broadband = "";
   const handleSetSearchOverlay = (
     features: GeoJSON.FeatureCollection | undefined
   ) => {
@@ -19,6 +21,7 @@ function App() {
       <MapBox />
       <div>
         <InputBox setState={handleSetSearchOverlay} />
+        <BroadBandBox broadband={broadband} />
       </div>
     </div>
   );
---
+++ front-end/src/components/BroadbandBox.tsx
@@ -0,0 +1,15 @@
+interface StatusInfo {
+  broadband: string;
+}
+
+export default function BroadBandBox({ broadband }: StatusInfo) {
+  return (
+    <div className="status-form">
+      {
+        <div>
+          <div>BroadBand = {broadband}</div>
+        </div>
+      }
+    </div>
+  );
+}
