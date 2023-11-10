f2debf2bd6b2c1529054648928b9f8d5447e8031
Saketh Dhulipalla <sakethdhulipalla@sakeths-mbp.devices.brown.edu>
2023-11-10 00:14:01 -0500

    fixing files

--- backend/src/main/java/edu/brown/cs/student/main/Handlers/JSONHandler.java
+++ backend/src/main/java/edu/brown/cs/student/main/Handlers/JSONHandler.java
@@ -1,20 +1,15 @@
 package edu.brown.cs.student.main.Handlers;
 
 import com.google.common.cache.Cache;
-import com.squareup.moshi.JsonAdapter;
 import com.squareup.moshi.JsonReader;
-import com.squareup.moshi.Moshi;
-import com.squareup.moshi.Types;
 import edu.brown.cs.student.main.Server.GeoJsonCollection;
 import edu.brown.cs.student.main.Server.GeoJsonCollection.Feature;
 import edu.brown.cs.student.main.Server.GeoJsonCollection.Geometry;
 import edu.brown.cs.student.main.Server.GeoJsonCollection.Properties;
 import edu.brown.cs.student.main.Server.JsonParsing;
-import java.lang.reflect.Type;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -36,55 +31,20 @@ public class JSONHandler implements Route {
       String maxLatitude = request.queryParams("maxLat");
       String minLongitude = request.queryParams("minLong");
       String maxLongitude = request.queryParams("maxLong");
-
-      Moshi moshi = new Moshi.Builder().build();
-      Type mapStringObject = Types.newParameterizedType(Map.class, String.class, Object.class);
-      JsonAdapter<Map<String, Object>> adapter = moshi.adapter(mapStringObject);
       JsonReader reader = JsonReader.of(new Buffer().writeUtf8(Files.readString(Path.of(
-          "src/main/java/edu/brown/cs/student/main/geodata/fullDownload.json"))));
+          "backend/src/main/java/edu/brown/cs/student/main/geodata/fullDownload.json"))));
       GeoJsonCollection geoFeature = JsonParsing.fromJsonGeneral(reader, GeoJsonCollection.class);
-
       if (minLatitude.isEmpty() || maxLatitude.isEmpty() || minLongitude.isEmpty() || maxLongitude.isEmpty()) {
-        Map<String,Object> responseMap = new HashMap<>();
-        responseMap.put("type", "success");
-        responseMap.put("data", JsonParsing.toJsonGeneral(geoFeature));
-        return adapter.toJson(responseMap);
+        return JsonParsing.toJsonGeneral(geoFeature);
       }
-      try {
-        Double minLat = Double.parseDouble(minLatitude);
-        Double maxLat = Double.parseDouble(maxLatitude);
-        Double minLong = Double.parseDouble(minLongitude);
-        Double maxLong = Double.parseDouble(maxLongitude);
-
-        if (minLat < -90.0 || minLat > 90.0 || maxLat < -90.0 || maxLat > 90.0 ||
-            minLong < -180.0 || minLong > 180.0 || maxLong < -180.0 || maxLong > 180.0) {
-          Map<String,Object> responseMap = new HashMap<>();
-          responseMap.put("result", "error_bad_request");
-          responseMap.put("message", "latitude must be between -90 and 90 & longitude must be between -180 and 180");
-          return adapter.toJson(responseMap);
-        }
-
-        geoFeature.features = filterFeatureByBoundingBox(geoFeature, minLat, maxLat, minLong, maxLong);
-        Map<String,Object> responseMap = new HashMap<>();
-        responseMap.put("type", "success");
-        responseMap.put("data", JsonParsing.toJsonGeneral(geoFeature));
-        return adapter.toJson(responseMap);
-      } catch (NumberFormatException e) {
-        Map<String, Object> responseMap = new HashMap<>();
-        responseMap.put("result", "error_bad_request");
-        responseMap.put("message", "min and max latitute and longitude must be valid double values");
-        return adapter.toJson(responseMap);
-      }
-
+      Double minLat = Double.parseDouble(minLatitude);
+      Double maxLat = Double.parseDouble(maxLatitude);
+      Double minLong = Double.parseDouble(minLongitude);
+      Double maxLong = Double.parseDouble(maxLongitude);
+      geoFeature.features = filterFeatureByBoundingBox(geoFeature, minLat, maxLat, minLong, maxLong);
+      return JsonParsing.toJsonGeneral(geoFeature);
     } catch(Exception e) {
-      Moshi moshi = new Moshi.Builder().build();
-      Type mapStringObject = Types.newParameterizedType(Map.class, String.class, Object.class);
-      JsonAdapter<Map<String, Object>> adapter = moshi.adapter(mapStringObject);
-      Map<String, Object> responseMap = new HashMap<>();
-      responseMap.put("result", "failure");
-      responseMap.put("error_type", "incorrect query format");
-      responseMap.put("error_description", "Search query must include: 'minLat', 'maxLat', 'minLong', and 'maxLong'");
-      return adapter.toJson(responseMap);
+      return e.getMessage();
     }
 
   }
